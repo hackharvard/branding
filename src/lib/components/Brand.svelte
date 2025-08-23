b49c2bfd61b6150906b8976703afcce124d25123
Yuen Ler Chow <lerchow@gmail.com>
2025-08-24 00:22:31 +0800

    update banner

--- src/lib/components/Brand.svelte
+++ src/lib/components/Brand.svelte
@@ -2,17 +2,7 @@
   <div
     class="dark:border-secondary inline-flex items-center gap-2 rounded-md border border-gray-200 p-2"
   >
-    <a
-      id="mlh-trust-badge"
-      style="display:block;max-width:100px;min-width:60px;position:fixed;left:50px;top:0;width:10%;z-index:10000"
-      href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2024-season&utm_content=white"
-      target="_blank"
-      ><img
-        src="https://s3.amazonaws.com/logged-assets/trust-badge/2024/mlh-trust-badge-2024-white.svg"
-        alt="Major League Hacking 2024 Hackathon Season"
-        style="width:100%"
-      /></a
-    >
+    <a id="mlh-trust-badge" style="display:block;max-width:100px;min-width:60px;position:fixed;left:50px;top:0;width:10%;z-index:10000" href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=white" target="_blank"><img src="https://s3.amazonaws.com/logged-assets/trust-badge/2026/mlh-trust-badge-2026-white.svg" alt="Major League Hacking 2026 Hackathon Season" style="width:100%"></a>
     <a class="shrink-0" href="/">
       <img class="ms-24 h-10" src="/logo.svg" alt="" />
     </a>
